5b9da76e43d7f8d94561032818a31fd7eddc5617
Arya Yunanta <arya.yunanta@ralali.com>
2023-08-21 19:52:49 +0700

    fix(short-url): update rule for short code url

--- internal/consts/validator_rules.go
+++ internal/consts/validator_rules.go
@@ -8,5 +8,6 @@ var (
 	RulesURL = []string{"required", "url", "min:3", "max:255"}
 
 	// RulesShortCodeURL rules
-	RulesShortCodeURL = []string{"required", "min:1", "max:255"}
+	// Example: RalaliBackend-2
+	RulesShortCodeURL = []string{"required", "regex:^[a-zA-Z0-9-]{1,255}$", "min:1", "max:255"}
 )
